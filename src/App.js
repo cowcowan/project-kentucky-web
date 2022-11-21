7483d0be17eabc40f46f732fcf4c419d2803ad5c
xy <cowcowan@gmail.com>
2022-11-20 21:28:42 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 import Signup from "./components/Signup";
+import hospital from "./img/hospital.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <Signup />
-    </div>
-  );
+const styles = {
+  header: {
+    backgroundImage: `url(${hospital})`,
+    height: "100vh",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+
+  content: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.3)",
+    // color: "white",
+  },
+};
+class App extends Component {
+  render() {
+    return (
+      <div style={styles.header}>
+        <div style={styles.content}>
+          <Signup />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
